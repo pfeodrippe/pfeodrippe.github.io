3e61a214ea270ecbd6048fd7eb944d6d70aac8fd
Paulo Feodrippe <pfeodrippe@gmail.com>
2019-12-14 19:53:43 -0300

    rebuilding site Sat Dec 14 19:53:43 -03 2019

--- fix-adoc.js
+++ fix-adoc.js
@@ -8,33 +8,8 @@ window.addEventListener('load', function () {
         return elm.getElementsByTagName('p')[0].innerHTML
     }
 
-    const admonitions = document.getElementsByClassName('admonition-block')
-    for (let i = admonitions.length - 1; i >= 0; i--) {
-        const elm = admonitions[i]
-        const type = getAdmonitionType(elm)
-        const text = getAdmonitionText(elm)
-        const parent = elm.parentNode
-        const tempDiv = document.createElement('div')
-        tempDiv.innerHTML = `<div class="admonitionblock ${type}">
-    <table>
-      <tbody>
-        <tr>
-          <td class="icon">
-            <i class="fa icon-${type}" title="${type}"></i>
-          </td>
-          <td class="content">
-            <div>
-            ${text}
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>`
 
-        const input = tempDiv.childNodes[0]
-        parent.replaceChild(input, elm)
-    }
 
     const doc_sections = document.getElementsByClassName('doc-sections');
+    console.log(doc_sections);
 })
